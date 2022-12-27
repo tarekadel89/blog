6ac31949892e59482677aad760ad55cf63e75cd5
Tarek <tarekadel89@gmail.com>
2022-12-27 23:46:43 +1000

    blog with database

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://127.0.0.1:27017/blogDB';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -8,60 +11,84 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let allPosts = [];
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const Post = mongoose.model("Post", {
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  }
+});
 
-app.get("/", function(req, res){
-  //console.log(allPosts);
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    allPosts: allPosts
+
+app.get("/", function (req, res) {
+  Post.find({}, function (error, documents) {
+    if (error) {
+      console.log("Error retrieving blogs from database");
+    } else {
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        allPosts: documents
+      });
+    }
   });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about", {
     aboutContent: aboutContent
   });
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", function (req, res) {
   res.render("contact", {
     contactContent: contactContent
   });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const postContent = {
-    postTitle : req.body.postTitle,
-    postBody: req.body.postBody
-  };
-  //console.log(postContent);
-  allPosts.push(postContent);
-  res.redirect("/");
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody
+  });
+  post.save().then(function () {
+    res.redirect("/");
+  });
 });
 
-app.get("/post/:postId", function(req, res){
-  const postId = req.params.postId - 1;
-  if(postId < allPosts.length) {
-    res.render("post", {
-      postTitle: allPosts[postId].postTitle,
-      postBody: allPosts[postId].postBody
-    }); } else {
+app.get("/post/:postId", function (req, res) {
+  const postId = String(req.params.postId);
+
+  Post.findById(postId, function (error, document) {
+    if (error) {
+      console.log("Error retrieveing blog from database");
       res.redirect("/");
+    } else {
+      if (document) {
+        res.render("post", {
+          postTitle: document.title,
+          postBody: document.body
+        });
+      } else {
+        res.redirect("/");
+      }
+
     }
+  });
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
